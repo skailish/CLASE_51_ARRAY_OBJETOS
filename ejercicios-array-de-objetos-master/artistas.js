8ce9309f32859290a4beb9feb7779d9abd9524fd
Daniela Capponi <capponi.daniela@gmail.com>
2020-03-17 14:05:53 -0300

    agrego consignas

--- ejercicios-array-de-objetos-master/artistas.js
+++ ejercicios-array-de-objetos-master/artistas.js
@@ -1,1 +1,19 @@
-// Hacer ejercicios de artistas acá
+// // Hacer ejercicios de artistas acá
+// Artistas
+// artistasSolistas, que tome por parámetro un array de artistas y devuelva un array con les artistas que sean solistas
+
+// artistasPorEdad, que tome un parámetro "edad" y un array de artistas y devuelva un array con les artistas que tengan dicha edad, ordenados de menor a mayor según la edad
+
+// cantidadDeArtistasPorInstrumento, que tome un parámetro "instrumento" y devuelva un objeto donde cada "instrumetno" es una propiedad y su valor la cantidad de artistas que tocan dicho instrumento
+
+// cantidadDeArtistasPorGenero, que tome por parámetro un array de artistas y devuelva un objeto donde cada "género" es una propiedad y su valor la cantidad de artistas de dicho género
+
+// artistasConMasDiscosQue, que tome por parámetro "cantidadDeDiscos" y un array de artistas y devuelva un array con les artistas que tiene más de esa cantidad de discos, ordenados de mayor a menor según cantidad de discos
+
+// artistaConMasEntradasVendidas, que tome por parámetro un array de artistas y devuelva el objeto artista que vendió más entradas en su último recital
+
+// artistaConMayorRecaudacion, que tome por parámetro un array de artistas y devuelva el objeto artista que más recaudó en su último recital(entradasVendidas * costoEntradas)
+
+// artistasConDiscoEnAnio, que tome por parámetro un parámetro "anio", y devuelva true un array con los artistas que tengan publicado al menos un disco en dicho año
+
+// artistaConMasCopias, que devuelva el objeto artista que más copias de discos en total vendió
--- ejercicios-array-de-objetos-master/estudiantes.js
+++ ejercicios-array-de-objetos-master/estudiantes.js
@@ -1,1 +1,29 @@
-// Hacer ejercicios de estudiantes acá
+// // Hacer ejercicios de estudiantes acá
+// Estudiantes
+// Hacer las siguientes funciones:
+
+// estudiantesPorHechizo, que tome por parámetro el nombre de un hechizo y un array de estudiantes y devuelva un array con todos les estudiantes que tengan ese hechizo como hechizoPreferido
+
+// estudiantesConMasAmiguesQue, que tome por parametro un numero y un array de estudiantes y devuelva un array con todos les estudiantes que tengan un número de amigues mayor o igual a el número pasado por parámetro
+
+// estudiantesConFamiliares, que tome por parámetro un array con nombres de familiares(p.ej, ["Sapo", "Lechuza"]) y un array de estudiantes y devuelva un array con les estudiantes que tengan cuyo familiar sea alguno de los incluidos en el parámetro
+
+// obtenerPromedioDeEstudiante, que tome por parámetro une estudiante(que se saca del array estudiantes) y devuelva el promedio total de todas las materias
+
+// estudiantesConPromedioMayorA, que tome por parámetro un número y un array de estudiantes y devuelva un array con les estudiantes que tengan un promedio total mayor a dicho número(usar la función anterior)
+
+// mejoresEstudiantesPorCasa, que tome por parámetro el nombre de una casa y un array de estudiantes y devuelva les estudiantes de dicha casa cuyo promedio total es mayor a 6
+
+// casaConMejoresEstudiantes, que tome por parámetro un array de estudiantes y devuelva el nombre de la casa que tiene más cantidad de estudiantes con promedio total mayor a 6(usar la función anterior)
+
+// estudiantesPorMateriaAprobada, que tome por parámetro el nombre de una materia y un array de estudiantes y devuelva una array con les estudiantes que tienen en dicha materia un promedio superior a 6
+
+// obtenerInfoResumida, que tome por parámetro un array de estudiantes y devuelva un array con objetos, habiendo un objeto por estudiante, donde cada objeto tiene las siguientes propiedades: nombre, casa, promedio, amigues(cantidad)
+
+// cantidadDeEstudiantesPorCasa, que tome por parámetro un array de estudiantes y devuelva un objeto con los nombres de las casas como propiedades y la cantidad de estudiantes por casa(no debe contar amigues)
+
+// cantidadDeEstudiantesPorMateriaAprobada, que tome por parámetro un array de estudiantes y devuelva un objeto con los nombres de las materias como propiedades y la cantidad de estudiantes que aprobaron dicha materia(promedio superior a 6)
+
+// promedioPorMateria, que tome por parámetro un array de estudiantes y devuelva un objeto con los nombres de las materias como propiedades y el promedio total de dicha materia entre todes les estudiantes(suma de todos los promedios divido la cantidad de estudiantes)
+
+// familiarPreferido, que tome por parámetro un array de estudiantes y devuelva el familiar que más estudiantes tienen
